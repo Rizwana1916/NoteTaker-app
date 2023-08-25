fc73e4bd0b6646818d56a151efe89fced9b39e05
Rizwana Sheikh <121599047+Rizwana1916@users.noreply.github.com>
2023-08-25 16:00:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,128 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 from pymongo import MongoClient
+import bcrypt
+from bson import ObjectId
 
 app = Flask(__name__)
+# Set the secret key for the Flask app
+app.secret_key = "your_secret_key"
 
-# Configure MongoDB connection
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/note_app'
-mongo = MongoClient(app.config['MONGO_URI'])
-db = mongo.Note_app
-notes_collection = db.notes
+# Initialize LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/')
+# Set the login view and unauthorized access message
+login_manager.login_view = "login"
+login_manager.login_message = "Please log in to access this page."
+
+# Define the user_loader function
+@login_manager.user_loader
+def load_user(user_id):
+    user = db.users.find_one({"_id": ObjectId(user_id)})
+    if user:
+        return User(user_id)
+    return None
+
+client = MongoClient("mongodb+srv://your_connection_string")
+db = client["Note_app"]
+users = db["users"]
+notes = db["notes"]
+
+@app.route("/")
 def index():
-    notes = notes_collection.find()
-    return render_template('index.html', notes=notes)
+    if "user_id" in session:
+        user_notes = notes.find({"user_id": session["user_id"]})
+        return render_template("index.html", notes=user_notes)
+    return redirect(url_for("login"))
 
-@app.route('/add', methods=['POST'])
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        users.insert_one({"username": username, "password": hashed_password})
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Retrieve user data from the 'users' collection
+        user_data = db.users.find_one({"username": username})
+
+        if user_data and bcrypt.checkpw(password.encode("utf-8"), user_data["password"]):
+            session["user_id"] = str(user_data["_id"])
+            return redirect(url_for("notes"))
+
+    return render_template("login.html")
+
+@app.route("/notes")
+def notes():
+    if "user_id" in session:
+        user_notes = db.notes.find({"user_id": session["user_id"]})
+        return render_template("index.html", notes=user_notes, current_user=current_user)
+    return redirect(url_for("login"))
+
+@app.route("/add_note", methods=["GET", "POST"])
 def add_note():
-    title = request.form.get('title')
-    content = request.form.get('content')
-    notes_collection.insert_one({'title': title, 'content': content})
-    return redirect(url_for('index'))
+    if "user_id" in session:
+        if request.method == "POST":
+            note_content = request.form["note_content"]
+            task = request.form["task"]  # Get the task from the form
+            time = request.form["time"]  # Get the time from the form
+            db.notes.insert_one({
+                "user_id": session["user_id"],
+                "task": task,              # Insert the task field
+                "content": note_content,
+                "time": time               # Insert the time field
+            })
+            return redirect(url_for("notes"))
+        return render_template("add_note.html")
+    return redirect(url_for("login"))
 
-@app.route('/edit/<note_id>', methods=['GET', 'POST'])
+@app.route("/edit_note/<note_id>", methods=["GET", "POST"])
 def edit_note(note_id):
-    note = notes_collection.find_one({'_id': note_id})
-    if request.method == 'POST':
-        new_title = request.form.get('title')
-        new_content = request.form.get('content')
-        notes_collection.update_one({'_id': note_id}, {'$set': {'title': new_title, 'content': new_content}})
-        return redirect(url_for('index'))
-    return render_template('edit.html', note=note)
+    if "user_id" in session:
+        note_collection = db["notes"]
+        note = note_collection.find_one({"_id": ObjectId(note_id)})
 
-@app.route('/delete/<note_id>')
+        if note and note["user_id"] == session["user_id"]:
+            if request.method == "POST":
+                updated_task = request.form["task"]
+                updated_content = request.form["updated_content"]
+                updated_time = request.form["time"]
+
+                note_collection.update_one(
+                    {"_id": ObjectId(note_id)},
+                    {
+                        "$set": {
+                            "task": updated_task,
+                            "content": updated_content,
+                            "time": updated_time
+                        }
+                    }
+                )
+                return redirect(url_for("notes"))
+
+            return render_template("edit.html", note=note)
+
+    return redirect(url_for("login"))
+
+@app.route("/delete_note/<note_id>", methods=["POST", "DELETE"])
 def delete_note(note_id):
-    notes_collection.delete_one({'_id': note_id})
-    return redirect(url_for('index'))
+    if "user_id" in session:
+        note_collection = db["notes"]
+        note = note_collection.find_one({"_id": ObjectId(note_id)})
 
-if __name__ == '__main__':
+        if note and note["user_id"] == session["user_id"]:
+            note_collection.delete_one({"_id": ObjectId(note_id)})
+            return redirect(url_for("notes"))
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
     app.run(debug=True)
